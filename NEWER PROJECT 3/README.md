61fae56c542a7cd02a16331afa25c08dfe14a9bd
jelle214 <6045325@mborijnland.nl>
2024-12-11 09:16:24 +0100

    changed even more

---
+++ NEWER PROJECT 3/README.md
@@ -0,0 +1,3 @@
+https://www.figma.com/design/mSPKtpNgkXMiptnROW2pCC/stijlgids?node-id=1-3&t=3uwIFsvGsNMeZmzl-1      <----- figma stijlgids jelle link
+
+https://www.figma.com/design/XJ1tj2SGtdzSjvRb3y3Ew3/Untitled?node-id=0-1&node-type=canvas&t=PBprVDqRIDF0RMzw-0 <-------- wireframes figma jelle
---
+++ NEWER PROJECT 3/review 1/php/undertale.php
@@ -0,0 +1,41 @@
+<?php
+// Associatieve arrays voor games, inclusief de afbeelding
+$games = [
+    1 => [
+        'title' => 'luigi`s mansion 3',
+        'genre' => 'adventure-puzzle',
+        'release_year' => 2019,
+        'developer' => 'Nintendo',
+        'image' => 'lw3.png' // Voeg hier de afbeelding toe
+    ],
+    2 => [
+        'title' => 'Undertale',
+        'genre' => 'Action-Adventure',
+        'release_year' => 2015,
+        'developer' => 'Toby Fox',
+        'image' => 'ut.png' // Voeg hier de afbeelding toe
+    ],
+];
+
+// Verkrijg de game_id uit de URL of zet het naar een standaardwaarde (bijv. 1)
+$game_id = isset($_GET['game_id']) ? $_GET['game_id'] : 1;
+
+// Controleer of het game_id bestaat in de array
+if (isset($games[$game_id])) {
+    $game = $games[$game_id];
+    echo "<h1>{$game['title']}</h1>";
+    echo "<p><strong>Genre:</strong> {$game['genre']}</p>";
+    echo "<p><strong>Release Year:</strong> {$game['release_year']}</p>";
+    echo "<p><strong>Developer:</strong> {$game['developer']}</p>";
+    // Toon de afbeelding
+    echo "<img src='foto/{$game['image']}' alt='{$game['title']}' style='width:300px; height:auto;'>";
+} else {
+    echo "<p>Game niet gevonden!</p>";
+}
+?>
+
+<!-- Links om andere games te bekijken -->
+<ul>
+    <li><a href="?game_id=1">luigi`s mansion 3</a></li>
+    <li><a href="?game_id=2">Undertale</a></li>
+</ul>
